56da3d2ce52d72ffb2b9e5375210e36bab68573d
Andrey Helldar <helldar@dragon-code.pro>
2024-03-15 23:33:54 +0300

    Fixed tests for Laravel 10

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,5 +4,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class)
+    ->beforeAll(function () {
+        if (!file_exists($database = databasePath())) {
+            file_put_contents($database, '');
+        }
+    })
     ->compact()
     ->in('Unit');
