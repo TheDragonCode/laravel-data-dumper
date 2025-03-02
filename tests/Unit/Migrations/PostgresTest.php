f898ed09d0cb55962faa6cd043eb94522217f8a4
andrey-helldar <10347617+andrey-helldar@users.noreply.github.com>
2025-03-02 19:21:26 +0000

    🦋 The code style has been fixed

--- tests/Unit/Migrations/PostgresTest.php
+++ tests/Unit/Migrations/PostgresTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 test('checks an attempt to export the migration table from postgres', function () {
     expect('COPY public.%s')->toBeDumped(1);
 })->group('Postgres');
